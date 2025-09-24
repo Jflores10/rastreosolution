a0ee1f2fd81e03094daa7a8c02eb0334744d49dd
Sbix04W <xavier12joh@hotmail.com>
2025-09-24 02:32:04 -0500

    ajuste

--- trackingsystemwebapp/app/PuntosRecorrido.php
+++ trackingsystemwebapp/app/PuntosRecorrido.php
@@ -13,9 +13,7 @@ class PuntosRecorrido extends Moloquent
         'latitud','longitud','unidad_id','pto_control_id',
         'fecha','tipo'
     ];
-    protected $casts = [
-        'fecha' => 'datetime',
-    ];
+  
 
     public function unidad()
     {
