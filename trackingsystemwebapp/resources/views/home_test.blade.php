878e61a640c05265e899ce54df41889cdd62e3f2
Sbix04W <xavier12joh@hotmail.com>
2026-02-20 15:40:46 -0500

    ajuste TVV22A

--- trackingsystemwebapp/resources/views/home_test.blade.php
+++ trackingsystemwebapp/resources/views/home_test.blade.php
@@ -1982,7 +1982,9 @@ $("#velocimetro").myfunc({divFact:10});
 
     function setMarcadorUnidad(unidad,fecha_gps_,fecha_servidor,_is)
     {
-        console.log(JSON.stringify(unidad));
+        console.log(JSON.stringify(unidad.latitud));
+        console.log(JSON.stringify(unidad.longitud));
+
         var estado;
         switch(unidad.estado_movil)
         {
