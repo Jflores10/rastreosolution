bcbe3b52b8fad703afee0459d811b0257b252e32
Sbix04W <xavier12joh@hotmail.com>
2026-02-26 00:18:32 -0500

    ajuste TRNUEVOV2_9

--- parseador/trackingsystem.js
+++ parseador/trackingsystem.js
@@ -1421,7 +1421,7 @@ function onClientConnected(socket) {
                                     ? String(document.cooperativa_id).trim()
                                     : null
                             });
-                            
+
                         } else {
                             dbTrackingSystem.collection('unidads').updateOne({
                                 _id: document._id
@@ -1438,6 +1438,20 @@ function onClientConnected(socket) {
                                 if (err)
                                     console.log(err);
                             });
+
+                            
+                            enviarALaravelPorWS({
+                                    type: 'unidad.alerta.puerta',
+                                    unidad_id: document._id,
+                                    imei: document.imei,
+                                    puerta: 'TRASERA',
+                                    estado: puerta,
+                                    fecha: fecha_gps,
+                                    cooperativa_id: document.cooperativa_id
+                                        ? String(document.cooperativa_id).trim()
+                                        : null
+                            });
+                            
                         }
                     }
                 });
