83c452dfc7423e2ec518df9ba61d3716a735c849
Sbix04W <xavier12joh@hotmail.com>
2026-05-04 02:53:39 -0500

    prueba homev2--6

--- trackingsystemwebapp/resources/views/homev2.blade.php
+++ trackingsystemwebapp/resources/views/homev2.blade.php
@@ -1529,7 +1529,7 @@ function fechaGpsInstante(fg) {
     return d;
 }
 
-var _DEBUG_NS_IMEI = '867162025719444';
+var _DEBUG_NS_IMEI = '868789023172876';
 
 function excedeLimiteSinTrama30Min(unidad) {
     if (!unidad) return true;
@@ -1540,7 +1540,6 @@ function excedeLimiteSinTrama30Min(unidad) {
     var fechaGpsAjustada = new Date(d.getTime() - AJUSTE_HORAS_DEBUG);
     var fechaActualAjustada = new Date(ahora - AJUSTE_HORAS_DEBUG);
     var diffMs = ahora - d.getTime();
-    /*
     if (String(unidad.imei) === _DEBUG_NS_IMEI) {
         console.log('[NS-debug imei ' + _DEBUG_NS_IMEI + ']', {
             fecha_gps_raw: unidad.fecha_gps,
@@ -1553,7 +1552,6 @@ function excedeLimiteSinTrama30Min(unidad) {
             diferencia_min: (diffMs / 60000).toFixed(3)
         });
     }
-        */
     return diffMs > (1 * 60 * 1000);
 }
 
